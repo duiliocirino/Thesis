616ecab0819f4734ab6bf29e5f16623aa48aeeaf
duiliocirino <10621933@polimi.it>
2023-07-09 00:00:55 +0200

    Pre batch operations.

--- Exploration POMDP/utility.py
+++ Exploration POMDP/utility.py
@@ -1,9 +1,10 @@
 from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
+from scipy.stats import t
 
 
-def print_gridworld_with_policy(agent, figsize=(6, 6)):
+def print_gridworld_with_policy(agent, figsize=(6, 6), title="notitle"):
     plt.figure(figsize=figsize)
 
     for state in range(agent.env.state_space.n):
@@ -49,18 +50,54 @@ def print_gridworld_with_policy(agent, figsize=(6, 6)):
     plt.axis('off')
     plt.show()
 
-def print_heatmap(agent, states):
+def print_heatmap(agent, d_t, title):
     '''
     This method serves the purpose of printing a heatmap of the state distribution given a trajectory of states drawn by
     an agent acting inside an environment.
     :param agent: the agent that acted in the environment;
-    :param states: the states over the trajectory that has been drawn by the agent acting in the environment, these must
-        be passed in the form of a vector of probabilities of the length of the states of the underlying MDP.
+    :param d_t: the state visitation distribution.
     '''
-    d_t = agent.compute_state_distribution(states)
-    print(d_t)
-    heatmap_data = np.zeros((agent.env.grid_size, agent.env.grid_size))
+    # Create the subplot
+    ax = plt.gca()
+    # Check corridor
+    if hasattr(agent.env, 'length_corridor'):
+        heatmap_data = np.full((agent.env.grid_size + agent.env.length_corridor, agent.env.grid_size), np.nan)
+    else:
+        heatmap_data = np.full((agent.env.grid_size, agent.env.grid_size), np.nan)
+    # Prepare data for plotting
     for index in range(agent.env.state_space.n):
         col, row = agent.env.index_to_state(index)
-        heatmap_data[row][col] = d_t[col][row]
-    sns.heatmap(data=heatmap_data, annot=True)
+        heatmap_data[row][col] = d_t[index]
+    # Plot the heatmap
+    sns.heatmap(data=heatmap_data, annot=True, ax=ax)
+    ax.set_title(title)
+    plt.show()
+
+def plot_graph(n_run, n_episodes, list_entropies, list_true_entropies, confidence):
+    # Initialize the plotting vectors
+    fig, ax = plt.subplots(figsize=(30, 10))
+    plt.title("Big number of episodes normal learning rate, normal gaussian")
+    entropies_means = []
+    true_entropies_means = []
+    under_line = []
+    over_line = []
+
+    for i in range(n_episodes):
+        entropies_mean = np.mean(list_entropies[i])
+        true_entropies_mean = np.mean(list_true_entropies[i])
+        entropies_std = np.std(list_entropies[i])
+        freedom_deg = n_run - 1
+        t_crit = np.abs(t.ppf((1 - confidence) / 2, freedom_deg))
+        entropies_means.append(entropies_mean)
+        true_entropies_means.append(true_entropies_mean)
+        under_line.append(entropies_mean - entropies_std * t_crit / np.sqrt(n_run))
+        over_line.append(entropies_mean + entropies_std * t_crit / np.sqrt(n_run))
+
+    ax.clear()
+    ax.plot(entropies_means, label='Believed Entropy')
+    ax.plot(true_entropies_means, label='True Entropy')
+    ax.fill_between(np.arange(n_episodes), under_line, over_line, color='b', alpha=0.1)
+    ax.set_xlabel('Episode')
+    ax.set_ylabel('Entropy')
+    ax.legend()
+    fig.canvas.draw()
